0c2e76146189ad00a2323c4b47e36337abadafe3
Tyler Dailey <tkdailey11@gmail.com>
2024-01-13 16:22:45 -0700

    Added blanks for 2015-2022

---
+++ Sources/Solutions/2020/Days/Day25.swift
@@ -0,0 +1,30 @@
+import Foundation
+import Tools
+
+final class Day25Solver: DaySolver {
+    let dayNumber: Int = 25
+    
+    let useSampleInput: Bool = false
+    let expectedPart1SampleResult = 0
+    let expectedPart2SampleResult = "Merry Christmas 🎄"
+
+	let expectedPart1Result = 0
+	let expectedPart2Result = "Merry Christmas 🎄"
+
+    private var input: Input!
+
+    private struct Input {
+    }
+
+    func solvePart1() -> Int {
+        return 0
+    }
+
+    func solvePart2() -> String {
+		"Merry Christmas 🎄"
+    }
+
+    func parseInput(rawString: String) {
+        input = .init()
+    }
+}
